99eba578246aab6439908b257b5b6b7245d3ae99
wyatt <wppurking@gmail.com>
2022-03-19 07:23:51 +0800

    * 增加两个测试

---
+++ deployment/src/test/java/com/easyacc/hutch/SimpleConsumerTest.java
@@ -0,0 +1,43 @@
+package com.easyacc.hutch;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.easyacc.hutch.core.MessageProperties;
+import java.util.HashMap;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/** Created by IntelliJ IDEA. User: wyatt Date: 2022/3/19 Time: 07:23 */
+public class SimpleConsumerTest {
+  /** 测试获取最大重试次数代码 */
+  @Test
+  void testGetXDeathLatestCount() {
+    var props = new MessageProperties();
+    props.setHeader("CC", List.of("abc_test"));
+    props.setHeader("x-first-death-exchange", "hutch.schedule");
+    props.setHeader("x-first-death-reason", "expired");
+    props.setHeader("x-first-queue", "hutch_delay_5s");
+
+    // 多次执行, 信息会留存, 寻找其中 count 最大的即可
+    var c1 = new HashMap<String, Object>();
+    c1.put("reason", "expired");
+    c1.put("original-expiration", "5000");
+    c1.put("count", 1L);
+    c1.put("exchange", "hutch.schedule");
+    c1.put("routing-keys", List.of("hutch.schedule.5s", "abc_test"));
+
+    var c2 = new HashMap<String, Object>();
+    c2.put("reason", "expired");
+    c2.put("original-expiration", "5000");
+    c2.put("count", 3L);
+    c2.put("exchange", "hutch.schedule");
+    c2.put("routing-keys", List.of("hutch.schedule.5s", "abc_test"));
+    props.setHeader("x-death", List.of(c1, c2));
+
+    var s = new SimpleConsumer(null, null, null);
+    assertThat(s.getXDeathLatestCount(props)).isEqualTo(3L);
+
+    var props2 = new MessageProperties();
+    assertThat(s.getXDeathLatestCount(props2)).isEqualTo(0L);
+  }
+}
---
+++ deployment/src/test/java/com/easyacc/hutch/utils/HutchUtilsTest.java
@@ -0,0 +1,26 @@
+package com.easyacc.hutch.utils;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.easyacc.hutch.util.HutchUtils.Gradient;
+import org.junit.jupiter.api.Test;
+
+/** Created by IntelliJ IDEA. User: wyatt Date: 2022/3/19 Time: 07:21 */
+public class HutchUtilsTest {
+  @Test
+  void testGradientName() {
+    assertThat(Gradient.G5.toString()).isEqualTo("5s");
+    assertThat(Gradient.G5.queue()).isEqualTo("hutch_delay_queue_5s");
+    assertThat(Gradient.G5.fixdDelay()).isEqualTo(5000L);
+  }
+
+  @Test
+  void testBackOffDelay() {
+    assertThat(Math.pow(3, 0)).isEqualTo(1);
+    assertThat(Math.pow(3, 1)).isEqualTo(3);
+    assertThat(Math.pow(3, 2)).isEqualTo(9);
+    assertThat(Math.pow(3, 3)).isEqualTo(27);
+    assertThat(Math.pow(3, 4)).isEqualTo(81);
+    assertThat(Math.pow(3, 5)).isEqualTo(243);
+  }
+}
