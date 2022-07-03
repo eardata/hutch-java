240be9c16b88b047a2c4dc9e4ab27fc4d849b148
kenyon <kenyonduan@gmail.com>
2022-07-03 19:01:53 +0800

    *  完成 testPublishWithSchedule

--- deployment/src/test/java/com/easyacc/hutch/deployment/AbcConsumer.java
+++ deployment/src/test/java/com/easyacc/hutch/deployment/AbcConsumer.java
@@ -1,13 +1,35 @@
 package com.easyacc.hutch.deployment;
 
+import com.easyacc.hutch.Hutch;
 import com.easyacc.hutch.core.HutchConsumer;
 import com.easyacc.hutch.core.Message;
+import com.easyacc.hutch.core.Threshold;
 import java.lang.invoke.MethodHandles;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /** Created by IntelliJ IDEA. User: wyatt Date: 2022/3/15 Time: 16:46 */
 public class AbcConsumer implements HutchConsumer {
   public static AtomicInteger Timers = new AtomicInteger(0);
+
+  @Override
+  public Threshold threshold() {
+    return new Threshold() {
+      @Override
+      public int rate() {
+        return 1;
+      }
+
+      @Override
+      public int interval() {
+        return 1;
+      }
+
+      @Override
+      public void publish(String msg) {
+        Hutch.publish(AbcConsumer.class, msg);
+      }
+    };
+  }
 
   @Override
   public void onMessage(Message message) {
