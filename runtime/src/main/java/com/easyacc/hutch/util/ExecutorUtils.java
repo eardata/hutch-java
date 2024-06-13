1dac5a94f2b2dfd91e0980c0234022dc88d60736
wppurking <wppurking@gmail.com>
2024-06-14 00:03:33 +0800

    * Executor 直接关闭, 任务的保证, 不由 Executor 来维持

--- runtime/src/main/java/com/easyacc/hutch/util/ExecutorUtils.java
+++ runtime/src/main/java/com/easyacc/hutch/util/ExecutorUtils.java
@@ -1,15 +1,14 @@
 package com.easyacc.hutch.util;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 /** Created by IntelliJ IDEA. User: kenyon Date: 2023/12/8 Time: 18:10 */
 public class ExecutorUtils {
-  public static void close(ExecutorService service) {
-    if (service == null) {
-      return;
+  public static List<Runnable> closeNow(ExecutorService executor) {
+    if (executor == null) {
+      return List.of();
     }
-
-    // 因为关闭任务, 无法确保任务执行后能够发送消息给 mq, 所以也直接一并关闭
-    service.shutdownNow();
+    return executor.shutdownNow();
   }
 }
