bf0bf0423d416723cbb724943d32f6a43215f182
duan <kenyonduan@gmail.com>
2025-07-28 18:05:59 +0800

    *  @ConfigItem 已被弃用，迁移到 @ConfigMapping

--- runtime/src/main/java/com/easyacc/hutch/config/HutchConfig.java
+++ runtime/src/main/java/com/easyacc/hutch/config/HutchConfig.java
@@ -2,67 +2,67 @@ package com.easyacc.hutch.config;
 
 import com.easyacc.hutch.core.ErrorHandler;
 import com.easyacc.hutch.error_handlers.MaxRetry;
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.quarkus.runtime.annotations.StaticInitSafe;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
-import lombok.ToString;
 
 /** 提供配置 Hutch 的配置. 需要是 StaticInitSafe 状态, 能够在 static 的时候就开始处理 */
 @StaticInitSafe
-@ConfigRoot(name = "hutch", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
-@ToString
-public class HutchConfig {
-  private static List<ErrorHandler> errorHandlers;
+@ConfigMapping(prefix = "quarkus.hutch")
+@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+public interface HutchConfig {
+  List<ErrorHandler> ERROR_HANDLERS = null;
 
   /** 是否默认启动 */
-  @ConfigItem(defaultValue = "true")
-  public boolean enable;
+  @WithDefault("true")
+  boolean enable();
 
   /** Hutch 的 prefix 前缀名称 */
-  @ConfigItem(defaultValue = "hutch")
-  public String name;
+  @WithDefault("hutch")
+  String name();
 
   /** rabbitmq 的 virtualHost */
-  @ConfigItem(defaultValue = "/")
-  public String virtualHost;
+  @WithDefault("/")
+  String virtualHost();
 
   /** 用户名 */
-  @ConfigItem(defaultValue = "guest")
-  public String username;
+  @WithDefault("guest")
+  String username();
 
   /** 密码 */
-  @ConfigItem(defaultValue = "guest")
-  public String password;
+  @WithDefault("guest")
+  String password();
 
   /** 连接服务器 */
-  @ConfigItem(defaultValue = "127.0.0.1")
-  public String hostname;
+  @WithDefault("127.0.0.1")
+  String hostname();
 
   /** 是否使用 rabbitmq 的 quorum queue */
-  @ConfigItem(defaultValue = "false")
-  public boolean quorum;
+  @WithDefault("false")
+  boolean quorum();
 
   /** 连接端口 */
-  @ConfigItem(defaultValue = "5672")
-  public int port;
+  @WithDefault("5672")
+  int port();
 
   /** 用于 schedule 的 Redis URL */
-  @ConfigItem(defaultValue = "redis://localhost:6379")
-  public String redisUrl;
+  @WithDefault("redis://localhost:6379")
+  String redisUrl();
 
   /** 默认的 scheduleExecutor 的 thread pool 数量. 如果只有 1 个, 那么 scheudle 的任务执行时间过长, 会阻塞 */
-  @ConfigItem(defaultValue = "6")
-  public int schdulePoolSize;
+  @WithDefault("6")
+  int schdulePoolSize();
 
   /** 具体一个 Consumer 执行任务的时候的 ThreadPool 的大小. 如果 <=0, 则表示不限制线程数量 */
-  @ConfigItem(defaultValue = "40")
-  public int workerPoolSize;
+  @WithDefault("40")
+  int workerPoolSize();
 
   /**
    * 构建 Hutch 需要的 SharedExecutorService
@@ -70,7 +70,7 @@ public class HutchConfig {
    * @param size 并发的数量. 如果不设限制, 那么则 < 0, 否则 100 表示 100 并发线程数
    * @return
    */
-  public static ThreadPoolExecutor buildSharedExecutor(int size) {
+  static ThreadPoolExecutor buildSharedExecutor(int size) {
     // 这里使用 newCachedThreadPool 与 使用 VirtualThread 很类似了, 可以无止境的根据需要创建新的 Thread
     if (size <= 0) {
       return (ThreadPoolExecutor) Executors.newCachedThreadPool();
@@ -85,7 +85,7 @@ public class HutchConfig {
    * @param size
    * @return
    */
-  public static ScheduledExecutorService buildSharedScheduledExecutor(int size) {
+  static ScheduledExecutorService buildSharedScheduledExecutor(int size) {
     return Executors.newScheduledThreadPool(size);
   }
 
@@ -97,20 +97,30 @@ public class HutchConfig {
    *   <li>在 Quarkus 应用初始化的时候, 通过 HutchConfig.getErrorHandlers 获取实例, 将对应的 ErrorHandler 实例添加到有序队列中
    * </ul>
    */
-  public static List<ErrorHandler> getErrorHandlers() {
-    if (errorHandlers == null) {
-      errorHandlers = new ArrayList<>();
-      errorHandlers.add(new MaxRetry());
-    }
-    return errorHandlers;
+  static List<ErrorHandler> getErrorHandlers() {
+    return Carrier.getErrorHandlers();
   }
 
   /** 获取 RabbitMQ 的 uri, 暂时不支持 tls */
-  public String getUri() {
-    var vh = virtualHost;
-    if (virtualHost.equals("/")) {
+  default String getUri() {
+    var vh = this.virtualHost();
+    if (this.virtualHost().equals("/")) {
       vh = "";
     }
-    return String.format("amqp://%s:*@%s:%d/%s", username, hostname, port, vh);
+    return String.format("amqp://%s:*@%s:%d/%s", this.username(), this.hostname(), this.port(), vh);
+  }
+
+  /** 由于 interface 不支持非 final 的属性, 这里单独为 errorHandlers 准备一个容器 */
+  class Carrier {
+    private static List<ErrorHandler> errorHandlers;
+
+    /** 获取默认的 ErrorHandlers */
+    public static List<ErrorHandler> getErrorHandlers() {
+      if (errorHandlers == null) {
+        errorHandlers = new ArrayList<>();
+        errorHandlers.add(new MaxRetry());
+      }
+      return errorHandlers;
+    }
   }
 }
